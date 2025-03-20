b2fef04677bf710874252823466fd325a11b24bc
David Nienge <davidnienge@gmail.com>
2025-03-20 15:54:39 +0100

    Dev tools setup

---
+++ app/(front)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello world!</h1>
+      <Button className="cursor-pointer">Click Me</Button>
+    </div>
+  );
+}
